b197306380002ca4c594f4e22d49b9790fdc3d43
Dinura Jayatilleke <you@example.com>
2024-02-13 07:44:54 -0500

    identifiers
    1st >> first
    sum >> Sum

--- main.java
+++ main.java
@@ -1,15 +1,15 @@
 // Identifier Naming in Java
 
-public class Main {
+public class main {
   
   public static void main(String[] args) {
-    int 1st_num = 1;
+    int first_num = 1;
     int last_num = 10;
     int Sum = 0;
 
-    for(int i = 1st_num; i <= last_num; i++)
-      sum = sum + i;
+    for(int i = first_num; i <= last_num; i++)
+      Sum = Sum + i;
   
-    System.out.println("Sum = " + sum);
+    System.out.println("Sum = " + Sum);
   }
 }
